a5373333bf1acfe24f3ec0103b66525f9f5c8d8b
UnbredOrbKeKe <Melle.romp@upcmail.nl>
2021-12-01 13:04:25 +0100

    dd
    d

---
+++ Drank mixer app/Drank mixer app/KiesDrank.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Drank_mixer_app
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class KiesDrank : ContentPage
+    {
+        public KiesDrank()
+        {
+            InitializeComponent();
+        }
+
+        private void Cola_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Sonnema_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Drank mixer app/Drank mixer app/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Drank_mixer_app
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void BeginMix_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new KiesDrank());
+
+        }
+    }
+}
